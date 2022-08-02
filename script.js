77456644b9efc9df485c41cbf18947fe4d83335b
Bladam93 <106922100+Bladam93@users.noreply.github.com>
2022-08-02 11:06:02 +0200

    update

---
+++ script.js
@@ -0,0 +1,12 @@
+function replace() {
+    document.getElementById("div1").style.display="none";
+    document.getElementById("div2").style.display="block";
+    }
+    function replace2() {
+        document.getElementById("div2").style.display="none";
+        document.getElementById("div3").style.display="block";
+        }
+        function replace3() {
+            document.getElementById("div3").style.display="none";
+            document.getElementById("div4").style.display="block";
+            }
